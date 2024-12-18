afb50cbc78e9e0d802b977e082083c5c7f63f6e0
Mehwish <mehwishgul715@gmail.com>
2024-12-18 19:58:14 +0500

    commit

---
+++ news/adgm-fines-wazim-nazir-following-consumer-protection-bureau-intervention.php
@@ -0,0 +1,357 @@
+<!-- <?php
+
+include_once "./includes/mail.php";
+
+$contactFormSubmitted = false;
+if (isset($_POST['form-contact'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['msg'];
+
+    $subject = "Contact";
+
+    $body = "<strong>Name: </strong> $name" . "<br>" .
+        "<strong>Email: </strong> $email" . "<br>" .
+        "<strong>Phone: </strong> $phone" . "<br>" .
+        "<strong>Message: </strong> $message";
+
+    $recaptchaSecret = '6LeWW5YqAAAAAEjGUeFCrxd0-lBEUAAZR0v0q9tO';
+    $recaptchaResponse = $_POST['g-recaptcha-response'];
+
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptchaSecret&response=$recaptchaResponse");
+    $responseKeys = json_decode($response, true);
+
+    if ($responseKeys["success"]) {
+        // reCAPTCHA validated
+        $msg = sendMail($email, $name, $subject, $body);
+
+        $contactFormSubmitted = true;
+    } else {
+        // reCAPTCHA failed
+        $msg['status'] = 'error';
+        $msg['message'] = "Please complete the reCAPTCHA verification.";
+
+        $contactFormSubmitted = true;
+    }
+}
+
+
+?> -->
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>ADGM Fines Wazim Nazir Following Consumer Protection Bureau's Intervention - Consumer Alert News - (CPB)</title>
+    <meta name="description"
+        content="Stay updated with the Consumer Protection Bureau UK. Explore the latest consumer alerts, UK consumer laws, and protection tips from a trusted agency.">
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="canonical"
+        href="https://www.consumerprotectionbureau.co.uk/news/price-display-discrepancy-at-asada-store.php">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="../assets/images/favicon.ico">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
+
+<?php include 'assets/include/header.php'; ?>
+<main>
+
+    <section class="py-lg-5 py-4 bg-light about-hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 mx-auto news-wrapper p-0">
+                    <img src="assets/images/consumer-protection-bureau-alert-news.png" alt="" class="w-100 h-auto">
+
+                    <div class="px-lg-5 px-3">
+                        <h1 class="news-heading text-uppercase  my-3">
+                            <span class="text-alert">Justice Served: </span>
+                            ADGM Fines Wazim Nazir Following Consumer Protection Bureau's Intervention
+                            </span>
+                        </h1>
+                        <p class="text">
+                            In a significant victory for consumer protection and regulatory integrity, the Abu Dhabi
+                            Global Market (ADGM) Registration Authority has imposed financial penalties on HSQ Holdings
+                            Ltd, PL1 Holdings Ltd, and their director, Mohammed Wazim Nazir, for egregious breaches of
+                            regulatory compliance.
+                        </p>
+                        <p class="text">
+                            The penalties include a $114,000 fine on Nazir personally and $15,000 each on his companies
+                            HSQ and PL1. These entities were authorised as passive Special Purpose Vehicles (SPVs), yet
+                            they blatantly exceeded their scope by actively managing residential properties in the UAE.
+                            Adding insult to injury, Nazir submitted a voluntary strike-off application riddled with
+                            false information, concealing ongoing disputes with property owners and continuing
+                            operations post-application.
+                        </p>
+                        
+                        <div class="row mt-4 mb-4">
+                            <div class="col-lg-6">
+                            <img src="../assets/images/news/Hanover Square in JVC dubai.jpg"
+                            alt="Consumet Protection Bureau Uk Alert News about ADGM Fines Wazim Nazir Following Consumer Protection Bureau's Intervention "
+                            class="w-100 asada-image shadow ">
+                            </div>
+                            <div class="col-lg-6">
+                            <img src="../assets/images/news/Platinum One Dubai.jpg"
+                            alt="Consumet Protection Bureau Uk Alert News about ADGM Fines Wazim Nazir Following Consumer Protection Bureau's Intervention "
+                            class="w-100 asada-image shadow ">
+                            </div>
+
+                            <div class="col-lg-12 order-lg-2 order-1">
+                            
+                                <h3 class="text-md text-bold  text-primary w-700 text-start mt-4">
+                                    CPB's Role in Exposing the Fraud
+                                </h3>
+                                <p class="text">
+                                    This case came to light only after the Compliance and Risk Consultancy (CRC) group,
+                                    through its Consumer Protection Bureau (CPB), intervened. When Wazim Nazir sought to
+                                    voluntarily wind up HSQ and PL1, CRC raised formal objections and mobilised affected
+                                    property owners to challenge the application. Many owners provided statements and
+                                    depositions to ADGM with CPB's assistance, exposing Nazir's misconduct and systemic
+                                    violations. Without this organised and strategic push, Nazir and his entities might
+                                    have escaped scrutiny entirely.
+
+                                </p>
+                                <p class="text">
+                                    The ADGM Registration Authority's investigation unveiled Nazir's deliberate
+                                    misrepresentation and obstruction, culminating in hefty fines and public censure.
+                                    This is not just a victory for justice but also a stark reminder of the power of
+                                    unified consumer action when supported by organisations like CPB.
+                                </p>
+                                <div class="">
+                                    <h3 class="text-md text-bold text-primary w-700">ACW: The Next Battle for Consumer Rights
+                                    </h3>
+                                    <p class="text">
+                                        While this decision marks progress, the fight is far from over. The property
+                                        owners of HSQ and PL1 remain in a dire situation. Their apartments are managed
+                                        by ACW, owned by Wazim Nazir, a company operating under a deeply flawed Sales
+                                        and Purchase Agreement (SPA). This SPA includes abusive clauses, denying owners
+                                        any exit option and binding them to ACW's management for an astonishing 20
+                                        years.
+                                    </p>
+                                    <p class="text">
+                                        Most owners are angry and disappointed. Their properties are rented out, yet
+                                        they have received little to no ROI for several years. Only a small handful are
+                                        receiving minimal returns, which amount to little more than peanuts compared to
+                                        market expectations. This untenable situation has shattered trust and further
+                                        exacerbated financial losses for already frustrated owners.
+                                    </p>
+                                    <h3 class="text-md text-bold text-primary w-700">CPB Will Take Action</h3>
+                                    <p class="text">
+                                        The Consumer Protection Bureau, as part of the CRC Group, is committed to
+                                        ensuring that consumer rights prevail. We are launching a vigorous campaign to
+                                        challenge ACW's abusive management practices.
+                                    </p>
+                                    <p class="text">
+                                        We will not rest until:
+                                    </p>
+                                    <ol>
+                                        <li>
+                                            <p class="text mb-0 pb-0">
+                                                ACW is ousted as the manager of Hanover Square Central and Platinum One
+                                                (PL1) properties.
+                                            </p>
+                                        </li>
+
+                                        <li>
+                                            <p class="text mb-0 pb-0">
+                                                Dubai authorities step in to dismantle exploitative SPAs that lock
+                                                owners into long-term, one-sided agreements.
+
+                                            </p>
+                                        </li>
+                                        <li>
+                                            <p class="text mb-0 pb-0">
+                                                Property owners take over their apartments as they rightfully and
+                                                legally deserve.
+                                            </p>
+                                        </li>
+
+                                    </ol>
+                                </div>
+                                <p class="text">
+                                    This case highlights a growing need for stringent oversight, transparent management
+                                    agreements, and stronger consumer protection mechanisms in the UAE property sector.
+                                    CPB will continue working with DLD and other UAE authorities to fight relentlessly
+                                    for fairness, accountability, and justice for all affected property owners.
+                                </p>
+
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+    <section class="py-lg-5 py-4 ">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <h1 class="main-heading text-center text-uppercase mb-5 mt-lg-2">
+                        Visit Our Subsidiary Companies
+                    </h1>
+
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-3">
+                    <a href="https://www.compliance-experts.co.uk/" target="_blank"
+                        class="text-decoration-none text-dark">
+                        <div class="amenity-card d-flex justify-content-center align-items-center flex-column">
+                            <img src="../assets/images/CRC-logo.png" alt="CRC logo" style="width:130px; height:auto;">
+                            <h1 class="mt-4 text-center text-md" style=" font-weight:500;">Compilance and Risk
+                                Consultancy</h1>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-3">
+                    <a href="https://carehomecompliance.com/" target="_blank" class="text-decoration-none text-dark">
+                        <div class="amenity-card d-flex justify-content-center align-items-center flex-column">
+                            <img src="../assets/images/Care Home Compilance.png" alt="CHC logo"
+                                style="width:100px; height:auto;">
+                            <h1 class="mt-4 text-center text-md" style=" font-weight:500;">Care Home Compilance</h1>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- <section>
+        <div class="contact-area py-lg-5 py-4">
+            <div class="container py-4">
+                <div class="row ">
+                    <div class="col-lg-5 col-md-12">
+                        <div class="contact-text">
+                            <div class="title">
+                                <h2>CONTACT US</h2>
+                            </div>
+
+                            <p class>We’d love to hear from you.
+
+                                <b>Just choose the most convenient method and we’ll get back to you as soon as we
+                                    can.</b>
+
+
+                            </p>
+                            <span class="date">
+
+                                <div class="d-flex align-items-center date">
+                                    <i class="bi bi-telephone-fill me-2"></i>
+                                    <a class="date" href="tel:+44 (0)2035854002"> +44
+                                        (0)2035854002</a>
+                                </div>
+                                <div class="d-flex align-items-center date my-3">
+                                    <i class="bi bi-envelope-fill me-2"></i>
+                                    <a class="date pb-0 mb-0"
+                                        href="mailto:info@consumerprotectionbureau.co.uk">info@consumerprotectionbureau.co.uk
+                                    </a>
+                                </div>
+                                <div class="d-flex align-items-start date">
+                                    <i class="bi bi-geo-alt-fill me-2"></i>
+                                    <p class="date my-0 py-0"> 83 VICTORIA STREET
+
+                                        WESTMINSTER, LONDON,
+
+                                        SW1H 0HW
+                                    </p>
+                                </div>
+                            </span>
+
+
+                        </div>
+                    </div>
+                    <div class="col col-lg-7 col-md-12 col-sm-12">
+                        <div class="contact-content ps-lg-5">
+                        <div class="contact-form">
+                                <?php if (isset($_POST['form-contact'])): ?>
+                                <?php if ($msg['status'] == 'success'): ?>
+                                <p class="alert alert-success text-center"><?php echo $msg['message'] ?></p>
+                                <?php else: ?>
+                                <p class="alert alert-danger text-center"><?php echo $msg['message'] ?></p>
+                                <?php endif; ?>
+                                <?php endif; ?>
+                                <form method="post" class="contact-validation-active" id="contact-form"
+                                    novalidate="novalidate">
+                                    <div class="half-col">
+                                        <label for="Name" class="text-white">Name</label>
+                                        <input type="text" name="name" id="name" class="form-control"
+                                            placeholder="Your Name">
+                                    </div>
+                                    <div class="half-col">
+                                        <label for="phone" class="text-white">Phone Number</label>
+                                        <input type="text" name="phone" id="phone" class="form-control"
+                                            placeholder="Phone">
+                                    </div>
+                                    <div>
+                                        <label for="email" class="text-white">Email</label>
+                                        <input type="email" name="email" id="email" class="form-control"
+                                            placeholder="Email">
+                                    </div>
+                                    <input type="hidden" name="form-contact">
+
+                                    <div>
+                                        <label for="msg" class="text-white">Message</label>
+                                        <textarea class="form-control" name="msg" id="msg"
+                                            placeholder="Message"></textarea>
+                                    </div>
+                                    <div class="g-recaptcha" data-sitekey="6LeWW5YqAAAAAO7CXW7SvpYQih0o9w_XaILDCy3j"></div>
+
+                                    <div class="submit-btn-wrapper">
+                                        <button type="submit" name="contact" class="main-btn">Send</button>
+                                        <div id="loader">
+                                            <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+</main>
+
+<?php include 'assets/include/footer.php'; ?>
+<script>
+contactFormSubmitted = <?php echo json_encode($contactFormSubmitted); ?>;
+
+if (contactFormSubmitted) {
+    const element = document.getElementById("contact-form");
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth', // smooth scrolling
+            block: 'center' // align to the top of the element
+        });
+    }
+}
+</script>
+</body>
+
+</html>
