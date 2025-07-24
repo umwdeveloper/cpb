52502b4f359a2a080aab3e26ee61937130fdf986
Mehwish <mehwishgul715@gmail.com>
2025-07-24 15:48:01 +0500

    renaming blog

--- blog/index.php
+++ blog/index.php
@@ -1,17 +1,274 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!-- <?php
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+include_once "./includes/mail.php";
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+$contactFormSubmitted = false;
+if (isset($_POST['form-contact'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['msg'];
+
+    $subject = "Contact";
+
+    $body = "<strong>Name: </strong> $name" . "<br>" .
+        "<strong>Email: </strong> $email" . "<br>" .
+        "<strong>Phone: </strong> $phone" . "<br>" .
+        "<strong>Message: </strong> $message";
+
+    $recaptchaSecret = '6LeWW5YqAAAAAEjGUeFCrxd0-lBEUAAZR0v0q9tO';
+    $recaptchaResponse = $_POST['g-recaptcha-response'];
+
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptchaSecret&response=$recaptchaResponse");
+    $responseKeys = json_decode($response, true);
+
+    if ($responseKeys["success"]) {
+        // reCAPTCHA validated
+        $msg = sendMail($email, $name, $subject, $body);
+
+        $contactFormSubmitted = true;
+    } else {
+        // reCAPTCHA failed
+        $msg['status'] = 'error';
+        $msg['message'] = "Please complete the reCAPTCHA verification.";
+
+        $contactFormSubmitted = true;
+    }
+}
+
+
+?> -->
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Job Opportunties - (CPB)</title>
+    <meta name="description" content="">
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Gidugu&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="canonical" href="https://www.consumerprotectionbureau.co.uk/blog/index.php">
+
+    <link rel="icon" type="image/x-icon" href="../assets/images/favicon.ico">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
+
+<?php include 'assets/include/header.php'; ?>
+<main>
+    <section class="py-lg-5 py-4 bg-light about-hero">
+        <div class="container">
+            <div class="row justify-content-center align-items-start">
+                <div class="col-lg-10 mx-auto ">
+                    <h1 class="main-heading text-center text-uppercase  mt-2">
+                        JOB OPPORTUNITIES AT <br> CONSUMER PROTECTION BUREAU
+                    </h1>
+                    <p class="text-md text-center">
+                        Join us in our mission to protect consumers and promote fairness across the UK. At the
+                        Consumer Protection Bureau, we are dedicated to making a difference in the lives of consumers
+                        everywhere. Explore our current job openings and become part of a team that is committed to
+                        justice, integrity, and positive change.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="pb-lg-5 pb-4 bg-light position-relative " style="z-index: 1;">
+        <div class="container pb-5 ">
+            <div class="row mb-lg-0 mb-4">
+                <div class="col-lg-4 mb-3 mx-auto">
+                    <div class="news-card shadow-sm">
+                        <div class="news-card-image d-flex justify-content-center align-items-center"
+                            style="background-color:#000032;">
+                            <img src="../assets/images/jobs/Join the Fight for Fairness.png"
+                                style="object-fit:contain; object-position:center;" alt="join cpb  For Just £5 a Month!"
+                                class="">
+                        </div>
+                        <div class="news-card-text">
+                            <!-- <span>9 July 2025</span> -->
+                            <h2 class="mb-0 pb-0">
+                                Lead Fundraising for National Impact and Global Reach
+                            </h2>
+                            <p class="text mb-0 pb-0">
+                                <span>(22 July 2025)</span>
+                                <b>LONDON </b> –
+                                The Consumer Protection Bureau (CPB) is the UK’s independent voice for consumers—leading
+                                national campaigns, influencing policy, and fighting injustice across banking,
+                                transport, digital, and energy sectors.
+
+
+                            </p>
+                            <div class="news-card-btn mt-4 mb-2 text-end">
+                                <a href="cpb-welcomes-new-unified-water-regulator-end-ofwat.php" class="news-alert-btn">Read
+                                    More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <section class="py-lg-5 py-4 ">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <h1 class="main-heading text-center text-uppercase mb-5 mt-lg-2">
+                        Visit Our Subsidiary Companies
+                    </h1>
+
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-3">
+                    <a href="https://www.compliance-experts.co.uk/" target="_blank"
+                        class="text-decoration-none text-dark">
+                        <div class="amenity-card d-flex justify-content-center align-items-center flex-column">
+                            <img src="../assets/images/CRC-logo.png" alt="CRC logo" style="width:130px; height:auto;">
+                            <h1 class="mt-4 text-center text-md" style=" font-weight:500;">Compilance and Risk
+                                Consultancy</h1>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-3">
+                    <a href="https://carehomecompliance.com/" target="_blank" class="text-decoration-none text-dark">
+                        <div class="amenity-card d-flex justify-content-center align-items-center flex-column">
+                            <img src="../assets/images/Care Home Compilance.png" alt="CHC logo"
+                                style="width:100px; height:auto;">
+                            <h1 class="mt-4 text-center text-md" style=" font-weight:500;">Care Home Compilance</h1>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- 
+    <section>
+        <div class="contact-area py-lg-5 py-4">
+            <div class="container py-4">
+                <div class="row ">
+                    <div class="col-lg-5 col-md-12">
+                        <div class="contact-text">
+                            <div class="title">
+                                <h2>CONTACT US</h2>
+                            </div>
+
+                            <p class>We’d love to hear from you.
+
+                                <b>Just choose the most convenient method and we’ll get back to you as soon as we
+                                    can.</b>
+
+
+                            </p>
+                            <span class="date">
+
+                                <div class="d-flex align-items-center date">
+                                    <i class="bi bi-telephone-fill me-2"></i>
+                                    <a class="date" href="tel:+44 (0)2035854002"> +44
+                                        (0)2035854002</a>
+                                </div>
+                                <div class="d-flex align-items-center date my-3">
+                                    <i class="bi bi-envelope-fill me-2"></i>
+                                    <a class="date pb-0 mb-0"
+                                        href="mailto:info@consumerprotectionbureau.co.uk">info@consumerprotectionbureau.co.uk
+                                    </a>
+                                </div>
+                                <div class="d-flex align-items-start date">
+                                    <i class="bi bi-geo-alt-fill me-2"></i>
+                                    <p class="date my-0 py-0"> 83 VICTORIA STREET
+
+                                        WESTMINSTER, LONDON,
+
+                                        SW1H 0HW
+                                    </p>
+                                </div>
+                            </span>
+
+
+                        </div>
+                    </div>
+                    <div class="col col-lg-7 col-md-12 col-sm-12">
+                        <div class="contact-content ps-lg-5">
+                        <div class="contact-form">
+                                <?php if (isset($_POST['form-contact'])): ?>
+                                <?php if ($msg['status'] == 'success'): ?>
+                                <p class="alert alert-success text-center"><?php echo $msg['message'] ?></p>
+                                <?php else: ?>
+                                <p class="alert alert-danger text-center"><?php echo $msg['message'] ?></p>
+                                <?php endif; ?>
+                                <?php endif; ?>
+                                <form method="post" class="contact-validation-active" id="contact-form"
+                                    novalidate="novalidate">
+                                    <div class="half-col">
+                                        <label for="Name" class="text-white">Name</label>
+                                        <input type="text" name="name" id="name" class="form-control"
+                                            placeholder="Your Name">
+                                    </div>
+                                    <div class="half-col">
+                                        <label for="phone" class="text-white">Phone Number</label>
+                                        <input type="text" name="phone" id="phone" class="form-control"
+                                            placeholder="Phone">
+                                    </div>
+                                    <div>
+                                        <label for="email" class="text-white">Email</label>
+                                        <input type="email" name="email" id="email" class="form-control"
+                                            placeholder="Email">
+                                    </div>
+                                    <input type="hidden" name="form-contact">
+
+                                    <div>
+                                        <label for="msg" class="text-white">Message</label>
+                                        <textarea class="form-control" name="msg" id="msg"
+                                            placeholder="Message"></textarea>
+                                    </div>
+                                    <div class="g-recaptcha" data-sitekey="6LeWW5YqAAAAAO7CXW7SvpYQih0o9w_XaILDCy3j"></div>
+
+                                    <div class="submit-btn-wrapper">
+                                        <button type="submit" name="contact" class="main-btn">Send</button>
+                                        <div id="loader">
+                                            <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+</main>
+
+<?php include 'assets/include/footer.php'; ?>
+<script>
+contactFormSubmitted = <?php echo json_encode($contactFormSubmitted); ?>;
+
+if (contactFormSubmitted) {
+    const element = document.getElementById("contact-form");
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth', // smooth scrolling
+            block: 'center' // align to the top of the element
+        });
+    }
+}
+</script>
+</body>
+
+</html>
